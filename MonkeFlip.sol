4f329d9b2c7d143550db5c167a23bdf147248ba3
PJPannetier <pierre-jean.pannetier@etudiant.univ-rennes1.fr>
2021-12-09 23:05:57 +0100

    Ajout de l'achat d'annonce

--- MonkeFlip.sol
+++ MonkeFlip.sol
@@ -8,7 +8,9 @@ contract Utilisateur {
     string public tel;
     string public username;
     uint public nombreAnnonce;
+    uint public nombreAchats;
     bool public registered;
+
 
     constructor(address _adress, string memory _localisation, string memory _mail, string memory _tel, string memory _username) {
         adress = _adress;
@@ -24,6 +26,10 @@ contract Utilisateur {
     function setNombreAnnonce(uint nb) public {
         nombreAnnonce = nb;
     }
+    function nbrAchatsPlus() public returns(uint){
+        nombreAchats +=1;
+        return nombreAchats;
+    }
 }
 
 contract Annonce {
@@ -34,6 +40,8 @@ contract Annonce {
     string public localisation;
     string public modeAcheminement;
     Utilisateur public user;
+    bool public vendu;
+    address public userVendu;
 
     constructor(Utilisateur _user, string memory _titre, string memory _description, string memory _etat, uint _prix, string memory _modeAcheminement, string memory _localisation) {
         titre = _titre;
@@ -43,6 +51,8 @@ contract Annonce {
         localisation = _localisation;
         modeAcheminement = _modeAcheminement;
         user = _user;
+        vendu=false;
+        userVendu = address(0);
     }
 
     //Modifie une annonce
@@ -55,13 +65,21 @@ contract Annonce {
         modeAcheminement = _modeAcheminement;
     }
 
+    function setVendu(address _userVendu) public {
+        vendu = true;
+        userVendu = _userVendu;
+    }
+
 }
 
 contract MonkeFlip{
 
+    
     mapping(address => Utilisateur) public listeUtilisateurs;
     mapping(address => mapping(uint => Annonce)) public listeAnnonces;
+    mapping(address => mapping(uint => Annonce)) public listeHistoriqueAchat;
     mapping(address => bool) utilisateurExiste;
+
     event AnnonceCree(address createur, uint idAnnonce);
 
     modifier isRegistered() {
@@ -86,11 +104,26 @@ contract MonkeFlip{
         Annonce newAnnonce = new Annonce(listeUtilisateurs[msg.sender],_titre,_description,_etat, _prix,_modeAcheminement,_localisation);
         listeAnnonces[msg.sender][listeUtilisateurs[msg.sender].nombreAnnonce()] = newAnnonce;
         listeUtilisateurs[msg.sender].setNombreAnnonce(listeUtilisateurs[msg.sender].nombreAnnonce()+1);
+        emit AnnonceCree(msg.sender, listeUtilisateurs[msg.sender].nombreAnnonce());
     }
 
     //Modifie une annonce
     function modifierAnnonce(uint indexAnnonce, string memory _titre, string memory _description, string memory _etat, uint _prix, string memory _modeAcheminement, string memory _localisation) isRegistered public {
         listeAnnonces[msg.sender][indexAnnonce].modifierAnnonce(_titre,_description,_etat, _prix,_modeAcheminement,_localisation);
+    }
+
+    function acheterAnnonce(address userAnnonce, uint indexAnnonce) public payable isRegistered {
+        require(msg.value >= listeAnnonces[userAnnonce][indexAnnonce].prix(), "Montant incorrect");
+        listeHistoriqueAchat[msg.sender][listeUtilisateurs[msg.sender].nbrAchatsPlus()]=listeAnnonces[userAnnonce][indexAnnonce];
+        payable(userAnnonce).transfer(msg.value);
+        listeAnnonces[userAnnonce][indexAnnonce].setVendu(msg.sender);
+    }
+
+    function initAnnonces() public isRegistered {
+        creerAnnonce("Titre1","Desc1","Etat1",1000000000000000000,"modeAcheminement1","localisation1");
+        creerAnnonce("Titre2","Desc2","Etat2",2000000000000000000,"modeAcheminement2","localisation2");
+        creerAnnonce("Titre3","Desc3","Etat3",3000000000000000000,"modeAcheminement3","localisation3");
+        creerAnnonce("Titre4","Desc4","Etat4",4000000000000000000,"modeAcheminement4","localisation4");
     }
 
     /*
